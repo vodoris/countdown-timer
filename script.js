910f0ac668979ac1a46d2ecb910944b985eb4144
Matt Gilbert <mattgilbert042@gmail.com>
2022-03-31 15:32:44 -0500

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const hoursElement = document.getElementById('hours');
 const minutesElement = document.getElementById('minutes');
 const secondsElement = document.getElementById('seconds');
 
-const targetDate = "01/01/2022";
+const targetDate = new Date().getYear() + 1;
 
 function countdown() {
   const now = new Date();
